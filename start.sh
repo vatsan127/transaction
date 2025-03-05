96c09d129efce951cf2fb389bf50e0e604a7a327
Vatsan <vatsan127@gmail.com>
2025-03-05 17:40:21 +0530

    commit for setting up base project ...

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ mkdir -p target/dependency
 cd target/dependency && jar -xf ../*.jar && cd -
 
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-echo "Old Tag: $TIMESTAMP"
+echo "Old Binary Tag: $TIMESTAMP"
 
 docker tag transaction:latest transaction:$TIMESTAMP
 docker build -t transaction:latest .
